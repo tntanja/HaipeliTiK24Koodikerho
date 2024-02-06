d6aff0199eb7791ba3caf5fb4156220fef786cef
tntanja <tanjaa_88@hotmail.com>
2024-02-06 16:07:29 +0200

    6.2. ennen tuntia

--- Haipeli2DKkerhoTiK24/Assets/Scripts/Player/PlayerController.cs
+++ Haipeli2DKkerhoTiK24/Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public float moveSpeed = 5f;
+   private Vector2 move;
+   private Rigidbody2D body;
 
+   private void Awake() {
+      body = GetComponent<Rigidbody2D>();
+   }
+
+   // called every 1/30th of a second, used for physics
+   // private void FixedUpdate(){
+   //    Move();
+   // }
+
+   // // function that handles the moving stuff
+   // private void Move(){
+   //    //move = controls.Player.Move.ReadValue<Vector2>();
+   //    Vector2 movement = new Vector2(move.x, move.y) * moveSpeed * Time.fixedDeltaTime;
+   //    body.MovePosition(body.position + movement);
+   // }
    
 }
