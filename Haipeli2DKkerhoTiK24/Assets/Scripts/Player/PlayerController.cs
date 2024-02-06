5ba9768fbcb0837b4eb111170055bf7468aa31e0
tntanja <tanjaa_88@hotmail.com>
2024-02-06 17:09:20 +0200

    6.2.24 tunnin jälkeen pushattu

--- Haipeli2DKkerhoTiK24/Assets/Scripts/Player/PlayerController.cs
+++ Haipeli2DKkerhoTiK24/Assets/Scripts/Player/PlayerController.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
    public float moveSpeed = 5f;
-   private Vector2 move;
    private Rigidbody2D body;
+   private Master controls;
+   private Vector2 moveInput;
 
    private void Awake() {
+      controls = new Master();
       body = GetComponent<Rigidbody2D>();
    }
 
-   // called every 1/30th of a second, used for physics
-   // private void FixedUpdate(){
-   //    Move();
-   // }
+   // kutsutaan kun objekti aktiivinen
+   private void OnEnable() {
+      controls.Enable();   //aktivoi liikkumis näppäimet
+   }
 
-   // // function that handles the moving stuff
-   // private void Move(){
-   //    //move = controls.Player.Move.ReadValue<Vector2>();
-   //    Vector2 movement = new Vector2(move.x, move.y) * moveSpeed * Time.fixedDeltaTime;
-   //    body.MovePosition(body.position + movement);
-   // }
+   // kutsutaan kun objekti ei ole enää aktiivinen
+   private void OnDisable() {
+      controls.Disable();  // lopettaa näppäinten toiminnon
+   }
+
+   // käytetään fysiikkaan liittyvissä updatessa
+   private void FixedUpdate(){
+      Move();
+   }
+
+   // liikkumisen toiminnallisuus
+   private void Move(){
+      moveInput = controls.Player.Move.ReadValue<Vector2>();
+      Vector2 movement = new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+      body.MovePosition(body.position + movement);
+   }
    
 }
